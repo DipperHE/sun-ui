10fc5f8c1af53cbd95f27d808c16fbbcdc09652e
Dipper <1023128147@qq.com>
2022-09-25 17:58:13 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Dipper />
+</template>
+
+<script>
+import Dipper from "./components/Dipper.vue";
+
+export default {
+  name: "App",
+  components: {
+    Dipper,
+  },
+};
+</script>
