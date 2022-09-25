61e27fb432afc1f691f8dec479a32ecfd5def254
Dipper <1023128147@qq.com>
2022-09-26 00:33:43 +0800

    完善aside切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@ import { ref, provide } from "vue";
 export default {
   name: "App",
   setup() {
-    const asideVisible = ref(false);
+    const width = document.documentElement.clientWidth;
+    const asideVisible = ref(width <= 500 ? false : true);
     provide("asideVisible", asideVisible);
   },
 };
--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 <script lang="ts">
@@ -41,12 +41,25 @@ export default {
       margin: 0 1em;
     }
   }
+  > .toggleAside {
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
   @media (max-width: 500px) {
     > .menu {
       display: none;
     }
     > .logo {
       margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
