709f21135b77d6d70992b6aa6b0771146280e9fe
Dipper <1023128147@qq.com>
2022-09-29 23:57:40 +0800

    加载中 css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,6 +3,7 @@
   <h1>示例一</h1>
   <div>
     <Button>你好</Button>
+    <Button @click="onClick">你好</Button>
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
@@ -48,6 +49,11 @@
     <Button disabled>禁用按钮</Button>
     <Button theme="link" disabled>禁用链接按钮</Button>
     <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
   </div>
 </template>
 <script lang="ts">
