3b5fd6d340d8e4ea821319176dedaa6c62788647
Dipper <1023128147@qq.com>
2022-09-29 21:27:30 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>button示例</div>
+  <div>Button示例</div>
+  <h1>示例一</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+};
+</script>
