b9f1d1f6fe990ef16e62062cd9b456a74d1060c0
Dipper <1023128147@qq.com>
2022-09-30 16:16:59 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,13 @@
 <template>
   <div>dialog示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: {
+    Dialog,
+  },
+};
+</script>
