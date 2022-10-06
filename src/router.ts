a72db9287a8d138d292eae5cca9fe3a20d863ab0
Dipper <1023128147@qq.com>
2022-10-06 22:05:23 +0800

    事不过三：优化 markdown

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,8 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "./views/Home.vue";
 import Doc from "./views/Doc.vue";
-import Intro from "./views/Intro.vue";
-import Install from "./views/Install.vue";
-import GetStarted from "./views/GetStarted.vue";
+import { h } from "vue";
+import Markdown from "./components/Markdown.vue";
 import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
@@ -11,6 +10,8 @@ import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
 
 const history = createWebHashHistory();
+const md = (filename) =>
+  h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
 export const router = createRouter({
   history,
   routes: [
@@ -20,9 +21,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
-        { path: "intro", component: Intro },
-        { path: "get-started", component: GetStarted },
-        { path: "install", component: Install },
+        { path: "intro", component: md("intro") },
+        { path: "get-started", component: md("get-started") },
+        { path: "install", component: md("install") },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
