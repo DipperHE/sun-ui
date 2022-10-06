da19326b1d85b47ef132aff47fa50ce8bdc6833c
Dipper <1023128147@qq.com>
2022-10-07 02:41:35 +0800

    优化官网

--- src/components/DocDemo.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>Doc默认页面</div>
-</template>
--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,6 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-import DocDemo from "./components/DocDemo.vue";
 
 const history = createWebHashHistory();
 const md = (filename) =>
@@ -20,7 +19,7 @@ export const router = createRouter({
       path: "/doc",
       component: Doc,
       children: [
-        { path: "", component: DocDemo },
+        { path: "", redirect: "/doc/intro" },
         { path: "intro", component: md("intro") },
         { path: "get-started", component: md("get-started") },
         { path: "install", component: md("install") },
