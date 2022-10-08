9a255f0b9ac60ef4f5574cc6cfeb8e0007afb819
Dipper <1023128147@qq.com>
2022-10-08 19:51:38 +0800

    解决build时的问题

--- src/router.ts
+++ src/router.ts
@@ -7,10 +7,12 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
+import intro from "./markdown/intro.md";
+import getStarted from "./markdown/get-started.md";
+import install from "./markdown/install.md";
 
 const history = createWebHashHistory();
-const md = (filename) =>
-  h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
+const md = (string) => h(Markdown, { content: string, key: string });
 export const router = createRouter({
   history,
   routes: [
@@ -20,9 +22,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", redirect: "/doc/intro" },
-        { path: "intro", component: md("intro") },
-        { path: "get-started", component: md("get-started") },
-        { path: "install", component: md("install") },
+        { path: "intro", component: md(intro) },
+        { path: "get-started", component: md(getStarted) },
+        { path: "install", component: md(install) },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
