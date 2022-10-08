a859723e5fb33fe411270f9ae97077ecc17be5e4
Dipper <1023128147@qq.com>
2022-10-09 01:03:48 +0800

    修复 Tabs 在 Vue 3 正式版的 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -40,14 +40,19 @@ export default {
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     onMounted(() => {
-      watchEffect(() => {
-        const { width } = selectedItem.value.getBoundingClientRect();
-        indicator.value.style.width = width + "px";
-        const { left: left1 } = container.value.getBoundingClientRect();
-        const { left: left2 } = selectedItem.value.getBoundingClientRect();
-        const left = left2 - left1;
-        indicator.value.style.left = left + "px";
-      });
+      watchEffect(
+        () => {
+          const { width } = selectedItem.value.getBoundingClientRect();
+          indicator.value.style.width = width + "px";
+          const { left: left1 } = container.value.getBoundingClientRect();
+          const { left: left2 } = selectedItem.value.getBoundingClientRect();
+          const left = left2 - left1;
+          indicator.value.style.left = left + "px";
+        },
+        {
+          flush: "post",
+        }
+      );
     });
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
